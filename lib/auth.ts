40ab4d5cfdd3139131a46bfa2bf39ba07dd3f11a
kunkoala <azharffrahadian@gmail.com>
2024-05-06 19:51:09 +0200

    Add utility function for merging CSS classes

---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { Lucia } from "lucia";
+
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+const adapter = new PrismaAdapter(client.session, client.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      // set to 'true' when using https
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+// IMPORTANT!
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+  }
+}
