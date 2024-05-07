75d887176f78dbf34101978259b09c5887da7cc3
kunkoala <azharffrahadian@gmail.com>
2024-05-07 15:08:25 +0200

    chore: Update signup function to create user and set session cookie

--- lib/auth/actions.ts
+++ lib/auth/actions.ts
@@ -13,7 +13,6 @@ export async function signup(
   _: any,
   formData: FormData
 ): Promise<ActionResult> {
-
   const email = formData.get("email");
   if (typeof email !== "string" || email.length < 6 || email.length > 255) {
     return {
@@ -38,6 +37,40 @@ export async function signup(
       email: email,
     },
   });
+
+  if (existingUser) {
+    return {
+      error: "User already exists",
+    };
+  }
+
+  const userId = generateIdFromEntropySize(21);
+  const hashedPassword = await hash(password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  });
+
+  await prisma.user.create({
+    data: {
+      id: userId,
+      email: email,
+      password_hash: hashedPassword,
+    },
+  });
+
+  // TODO: Verification email
+
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  return redirect("/");
 }
 
 interface ActionResult {
