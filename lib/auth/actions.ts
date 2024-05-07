582996dc697e11d978deb4fb33115d19a15eaf14
kunkoala <azharffrahadian@gmail.com>
2024-05-07 15:17:15 +0200

    feat: Update login function to handle user authentication
    - Added login function to handle user authentication.
    - Implemented email and password validation.
    - Added logic to check if user exists and verify password.
    - Created session and set session cookie upon successful login.
    - Redirect user to dashboard after successful login.

--- lib/auth/actions.ts
+++ lib/auth/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { hash } from "@node-rs/argon2";
+import { hash, verify } from "@node-rs/argon2";
 import { cookies } from "next/headers";
 import { lucia } from "@/lib/auth/auth";
 import { redirect } from "next/navigation";
@@ -8,6 +8,10 @@ import { generateIdFromEntropySize } from "lucia";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+interface ActionResult {
+  error: string;
+}
 
 export async function signup(
   _: any,
@@ -73,6 +77,54 @@ export async function signup(
   return redirect("/");
 }
 
-interface ActionResult {
-  error: string;
+export async function login(_: any, formData: FormData): Promise<ActionResult> {
+  const email = formData.get("email");
+  if (typeof email !== "string" || email.length < 6 || email.length > 255) {
+    return {
+      error: "Invalid email",
+    };
+  }
+  const password = formData.get("password");
+  if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.length > 255
+  ) {
+    return {
+      error: "Invalid password",
+    };
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!existingUser) {
+    return {
+      error: "Incorrect Username or Password",
+    };
+  }
+
+  const passwordMatch = await verify(existingUser.password_hash, password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  });
+  if (!passwordMatch) {
+    return {
+      error: "Incorrect Username or Password",
+    };
+  }
+
+  const session = await lucia.createSession(existingUser.id, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+  return redirect("/dashboard");
 }
